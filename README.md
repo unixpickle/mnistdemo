4d09a005684185e3d47d2f672e229461ad96d005
Alex Nichol <unixpickle@gmail.com>
2016-07-21 18:42:25 -0400

    wrote README and generic interfaces

---
+++ README.md
@@ -0,0 +1,3 @@
+# mnist-demo
+
+The purpose of this project is to manually test different classifiers on MNIST. My [mnist package](https://github.com/unixpickle/mnist) the training and testing of classifiers on MNIST, but it is not interactive. This project serves to augment the mnist package with interactive demos.
---
+++ interfaces.go
@@ -0,0 +1,43 @@
+package mnistdemo
+
+import "github.com/unixpickle/serializer"
+
+// Sample is a 28x28 image.
+// Pixels in the image are 1 if they're black and
+// 0 if they're white.
+type Sample [28 * 28]float64
+
+// A TrainingSample is an image sample with a
+// corresponding digit label.
+type TrainingSample struct {
+	Sample *Sample
+	Label  int
+}
+
+// A Classifier can be trained on a set of samples
+// and predicts labels for samples.
+type Classifier interface {
+	serializer.Serializer
+
+	Train(data, validation []*TrainingSample)
+	Classify(s *Sample) int
+}
+
+// A ClassifierDesc includes a plain-text description
+// of a classifier as well as a constructor for that
+// classifier.
+type ClassifierDesc struct {
+	Desc      string
+	Construct func() Classifier
+}
+
+// Classifiers stores ClassifierDescs for each available
+// classifier.
+var Classifiers = map[string]ClassifierDesc{
+	"forest": ClassifierDesc{
+		Desc: "random forests with ID3",
+		Construct: func() Classifier {
+			return &Forest{}
+		},
+	},
+}

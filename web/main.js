95ef1fe493b4187715c22a2a572dfd0d7c6ec76f
Alex Nichol <unixpickle@gmail.com>
2016-07-21 21:03:07 -0400

    implemented clear button
    In doing this, I realized that it's kind of annoying to have the clear button re-appear every time you draw something. Perhaps I will feel the same way about the classification cell once I get to that; we shall see.

--- web/main.js
+++ web/main.js
@@ -2,26 +2,22 @@
 
   window.app = {};
 
+  var clearButton = null;
+  var drawing = null;
+
   function initialize() {
-    var d = new window.app.Drawing();
-    d.onChange = function() {
-      // Code is usedful to see that the intensities are valid.
-      var bmp = d.mnistIntensities();
-      var canvas = document.createElement('canvas');
-      canvas.width = 28;
-      canvas.height = 28;
-      var ctx = canvas.getContext('2d');
-      ctx.fillStyle = 'black';
-      var idx = 0;
-      for (var y = 0; y < 28; ++y) {
-        for (var x = 0; x < 28; ++x) {
-          ctx.globalAlpha = 1 - bmp[idx++];
-          ctx.fillRect(x, y, 1, 1);
-        }
-      }
-      canvas.style.border = '1px solid black';
-      document.body.appendChild(canvas);
+    clearButton = document.getElementById('clear-button');
+    clearButton.addEventListener('click', clear);
+
+    drawing = new window.app.Drawing();
+    drawing.onChange = function() {
+      clearButton.className = '';
     };
+  }
+
+  function clear() {
+    drawing.reset();
+    clearButton.className = 'hidden';
   }
 
   window.addEventListener('load', initialize);
